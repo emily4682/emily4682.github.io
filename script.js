0ae9d899bab0fffa5328a830930905020aa016fd
emily4682 <159537768+emily4682@users.noreply.github.com>
2024-09-01 13:53:53 -0500

    Work on CV & Spacers

---
+++ script.js
@@ -0,0 +1,7 @@
+function spacer(){
+    document.write('<div> <svg class="spacer"> <defs> <pattern id="dashedFill" height="8" width="3" patternUnits="userSpaceOnUse"> <line x1="0" y1="4" x2="4" y2="4" stroke-width="1.5" stroke="black"/> </pattern> </defs> <rect height=100% width=100% fill="url(#dashedFill)"/> </svg> </div>')
+}
+
+function spacerSmallerWidth(){
+    document.write('<div> <svg class="spacer" style="padding-left: 5%;"> <defs> <pattern id="dashedFill" height="8" width="3" patternUnits="userSpaceOnUse"> <line x1="0" y1="4" x2="4" y2="4" stroke-width="1.5" stroke="black"/> </pattern> </defs> <rect height=100% width=90% fill="url(#dashedFill)"/> </svg> </div>')
+}
